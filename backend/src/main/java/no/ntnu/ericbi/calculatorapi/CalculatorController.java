1465bf5d67b72133cae6224c567836f662699a42
BeatsuDev <ericbieszczad@gmail.com>
2024-01-29 17:58:37 +0100

    add: logging to backend

--- backend/src/main/java/no/ntnu/ericbi/calculatorapi/CalculatorController.java
+++ backend/src/main/java/no/ntnu/ericbi/calculatorapi/CalculatorController.java
@@ -1,16 +1,19 @@
 package no.ntnu.ericbi.calculatorapi;
 
-import java.util.List;
+import java.util.logging.Logger;
 
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class CalculatorController {
     private final CalculationService calculationService = new CalculationService();
+    private static final Logger logger = Logger.getLogger( CalculatorController.class.getName() );
 
     @GetMapping("/calculate")
     public Calculation calculate(@RequestParam(value = "expression") String expression) {
-        Double answer = calculationService.evaluate(expression);
-        return new Calculation(expression, answer.toString());
+        logger.info("Received request to calculate " + expression);
+        double answer = calculationService.evaluate(expression);
+        logger.info("Returning answer " + answer);
+        return new Calculation(expression, Double.toString(answer));
     }
 }
